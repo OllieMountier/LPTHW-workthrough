dbf1641bf1da23929428959562334f9323a96291
OllieMountier <olliemountier@btinternet.com>
2023-08-02 14:24:51 +0100

    learning about simple maths problems

---
+++ Exercise_3.py
@@ -0,0 +1,28 @@
+# Exercise 3 is the first introduction to numbers and maths in Python. 
+
+# The drills are mostly simple math techniques but introduces floating point numbers
+# to the student for the first time.
+
+# The first task is to write a comment above each section to provide info on the task and
+# explain what ur doing
+
+#simulates counting hens, roosters and eggs using simple maths problems
+print("I will now count my chickens")
+print("Hens", 25+30/6)
+print("Roosters", 100-25*3%4)
+print("now I will count the eggs:")
+print(3+2+1-5+4%2-1/4+6)
+
+#uses Boolean to use Python as a calculator for calculations
+print("is it true that 3+2<5-7?")
+print(3+2<5-7)
+print("what is 3+2?", 3+2)
+print("what is 5-7?", 5-7)
+print("oh thats why its false")
+
+# after finding out the original equation is wrong, you are to test for the correct output
+# teaching you about more symbols
+print("how about some more")
+print("is it greater?, 5> -2")
+print("is it greater or equal?", 5>=-2)
+print("is it less or equal?", 5<=-2)
