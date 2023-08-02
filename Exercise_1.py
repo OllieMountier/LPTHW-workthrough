e5c6a4cd43e9c41463b4df5c91efb1b167db2784
OllieMountier <olliemountier@btinternet.com>
2023-08-02 12:15:42 +0100

    adding exercise 1 to the repo, it teaches basic function of python

---
+++ Exercise_1.py
@@ -0,0 +1,16 @@
+#Exercise 1- A good first program
+# Teaches about how to run a program. Fundamental work, I am only doing in order for full completion of the book.
+
+print("Hello World")
+print("Hello again")
+print("I like typing this")
+print("This is fun")
+print("Yay! printing")
+print("I'd much rather you 'not'.")
+print("'I said' do not touch this")
+
+#You are then tasked to run this file, a new line I will print below, and then just one of the lines
+#You are then told to put a "#" in front of one of the lines to see what happens. Obviously, I am already 
+#using comments so have not bothered.
+
+print("printing another line")
