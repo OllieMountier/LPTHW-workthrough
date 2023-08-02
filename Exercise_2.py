30ffba1623e5953d7858259b5cc7aff693b99ae7
OllieMountier <olliemountier@btinternet.com>
2023-08-02 12:32:18 +0100

    exercise 2 teaches comments

---
+++ Exercise_2.py
@@ -0,0 +1,6 @@
+# Exercise 2 teaches of comments and Pound characters. Although I have experience with these
+# it was interesting to find out they are called octothropes or "Pound characters"
+
+# The drills in this exercise are mostly theoretical but I will still upload this as it completes
+# the book rather than skipping. The drills include ensuring understanding of the comments and 
+# also reading code backwards so you learn to catch errors without attaching meaning to each part.
