8b3abfae36c1d8bc7edee55948744977db09ea66
OllieMountier <olliemountier@btinternet.com>
2023-08-03 12:36:36 +0100

    exercise 7 is further practice on exercise 6

---
+++ Exercise_7.py
@@ -0,0 +1,23 @@
+# This exercise is a continuation of the previous, but extends to more practice rather than learning. It is used to continue the students learning of combining strings together
+
+# Printing variables and initial use of maths on strings
+print("Mary had a litle lamb")
+print("it's fleece was a white as %s" % "snow")
+print("and everywhere that Mary went.")
+print("." * 10) 
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6)
+print(end7 + end8 + end9 +end10 + end11 + end12)
